f6443ceabb1ea5c23dc2ca24eaf3eebd30d35758
David Cramb <david.cramb@gmail.com>
2015-12-04 21:03:26 -0600

    adding scripting to image on hover

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 //ADD YOUR CUSTOM JAVASCRIPT AND jQUERY HERE
 $('.gallery').on('click','img', function() {
    $(this).animate({
-       'width':'50vh'
+       'width':'50vh',
+       'margin':'-10%'
    },500);
    $('img').not($(this)).animate({'opacity':'0.2'}, 500);
 });
+$('.row>img').hover(function(){
+  $(this).html('img class=alt=>' + '</div>');
+  console.log('this');
+})
